b9a8c7cb03cf6e24c0bf789f388433beb19e9d4b
Badamosi Majeed <badamosimajeed@gmail.com>
2021-08-22 14:00:07 +0100

    Menu and Navbar Completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: ['group-hover'],
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 }
