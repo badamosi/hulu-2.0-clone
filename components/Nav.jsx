5c966c4bd91157c4b8854c04e59bed992da4aeea
Badamosi Majeed <badamosimajeed@gmail.com>
2021-08-30 14:56:54 +0100

    Final Build

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/dist/client/router"
-import request from "../Utils/request"
+import requests from "../Utils/requests"
 
 function Nav() {
 
@@ -7,8 +7,8 @@ function Nav() {
     return (
         <nav className="relative">
             <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
-                 {Object.entries(request).map(([key, { title, url }]) => 
-                     <h2 key={key} onClick={()=> router.push(`/?genres=${key}`)} className="last:pr-24 cursor-pointer transition duration-100 
+                 {Object.entries(requests).map(([key, { title, url }]) => 
+                     <h2 key={key} onClick={()=> router.push(`/?genre=${key}`)} className="last:pr-24 cursor-pointer transition duration-100 
                     transform hover:scale-125 hover:text-white
                      active:text-red-500">{title}</h2>
                 )}
